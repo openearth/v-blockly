280a8ab51daa2933040d8a806387a31f9453f00b
Fedor Baart <fedor.baart@deltares.nl>
2019-07-05 16:54:22 +0200

    cleanup:

--- src/blockly.js
+++ src/blockly.js
@@ -6,7 +6,7 @@ var exports = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js
 
 window.Blockly = exports.Blockly
 window.goog = exports.goog
-export default Blockly
 // Import for side effects
 require('blockly/blocks_compressed.js')
 require('blockly/msg/js/en.js')
+export default Blockly
