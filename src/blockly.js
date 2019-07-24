36929a6e8f03320959c0c2ead9b187f4682c92f4
Fedor Baart <fedor.baart@deltares.nl>
2019-07-24 17:01:24 +0200

    pass variables through import-loader

--- src/blockly.js
+++ src/blockly.js
@@ -2,12 +2,12 @@
 // not all of blockly is converted to modern js yet
 // Import and use export module to extract Blockly variable
 // eslint-disable-next-line import/no-webpack-loader-syntax
-var exports = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js')
+var {Blockly, goog} = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js')
 // Expose it globaly so it can be changed by  other Blocks  import
 
-window.Blockly = exports.Blockly
-window.goog = exports.goog
-// Import for side effects
-require('blockly/blocks_compressed.js')
-require('blockly/msg/js/en.js')
-export default window.Blockly
+// Import with side effects
+// eslint-disable-next-line import/no-webpack-loader-syntax
+require('imports-loader?Blockly=>Blockly,goog=>goog!blockly/blocks_compressed.js')
+// eslint-disable-next-line import/no-webpack-loader-syntax
+require('imports-loader?Blockly=>Blockly,goog=>goog!blockly/msg/js/en.js')
+export default Blockly
