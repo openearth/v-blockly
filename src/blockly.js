d6d6e6c631d9187f935ac03e9750e0a5d7730ab5
Fedor Baart <fedor.baart@deltares.nl>
2019-07-24 15:19:55 +0200

    explicit export

--- src/blockly.js
+++ src/blockly.js
@@ -1,6 +1,7 @@
 // Import and make available as global variable
 // not all of blockly is converted to modern js yet
 // Import and use export module to extract Blockly variable
+// eslint-disable-next-line import/no-webpack-loader-syntax
 var exports = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js')
 // Expose it globaly so it can be changed by  other Blocks  import
 
@@ -9,4 +10,4 @@ window.goog = exports.goog
 // Import for side effects
 require('blockly/blocks_compressed.js')
 require('blockly/msg/js/en.js')
-export default Blockly
+export default window.Blockly
