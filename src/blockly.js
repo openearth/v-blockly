b6f5dbe66c49888181f53beede289c1e105ddb1b
Fedor Baart <fedor.baart@deltares.nl>
2019-07-24 17:08:36 +0200

    pass along imports through window

--- src/blockly.js
+++ src/blockly.js
@@ -4,10 +4,13 @@
 // eslint-disable-next-line import/no-webpack-loader-syntax
 var {Blockly, goog} = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js')
 // Expose it globaly so it can be changed by  other Blocks  import
+console.log('Blockly, goog', Blockly, goog)
+window.Blockly = Blockly
+window.goog = goog
 
 // Import with side effects
 // eslint-disable-next-line import/no-webpack-loader-syntax
-require('imports-loader?Blockly=>Blockly,goog=>goog!blockly/blocks_compressed.js')
+require('imports-loader?Blockly=>window.Blockly,goog=>window.goog!blockly/blocks_compressed.js')
 // eslint-disable-next-line import/no-webpack-loader-syntax
-require('imports-loader?Blockly=>Blockly,goog=>goog!blockly/msg/js/en.js')
+require('imports-loader?Blockly=>window.Blockly,goog=>window.goog!blockly/msg/js/en.js')
 export default Blockly
