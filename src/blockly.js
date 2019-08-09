c14bf7b02f9257b67c38d630083bb8f778313e48
Fedor Baart <fedor.baart@deltares.nl>
2019-08-09 14:09:20 +0200

    configure import  and exports

--- src/blockly.js
+++ src/blockly.js
@@ -2,15 +2,13 @@
 // not all of blockly is converted to modern js yet
 // Import and use export module to extract Blockly variable
 // eslint-disable-next-line import/no-webpack-loader-syntax
-var {Blockly, goog} = require('exports-loader?Blockly,goog!blockly/blockly_compressed.js')
+var { Blockly, goog } = require('blockly/blockly_compressed.js')
 // Expose it globaly so it can be changed by  other Blocks  import
 console.log('Blockly, goog', Blockly, goog)
-window.Blockly = Blockly
-window.goog = goog
 
 // Import with side effects
 // eslint-disable-next-line import/no-webpack-loader-syntax
-require('imports-loader?Blockly=>window.Blockly,goog=>window.goog!blockly/blocks_compressed.js')
+require('blockly/blocks_compressed.js')
 // eslint-disable-next-line import/no-webpack-loader-syntax
-require('imports-loader?Blockly=>window.Blockly,goog=>window.goog!blockly/msg/js/en.js')
+require('blockly/msg/js/en.js')
 export default Blockly
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,26 @@ module.exports = {
     output: {
       libraryExport: 'default'
     }
+  },
+  chainWebpack: config => {
+    // load exports
+    config.module
+      .rule('export')
+      .test(/blockly\/blockly_compressed\.js/)
+      .use('exports-loader?Blockly,goog')
+      .loader('exports-loader')
+      .end()
+    config.module
+      .rule('import')
+      .test(/blockly\/blocks_compressed\.js/)
+      .use('imports-loader?Blockly=>window.Blockly,goog=>window.goog')
+      .loader('imports-loader')
+      .end()
+    config.module
+      .rule('import')
+      .test(/blockly\/msg\/js\/en.js/)
+      .use('imports-loader?Blockly=>window.Blockly,goog=>window.goog')
+      .loader('imports-loader')
+      .end()
   }
 }
