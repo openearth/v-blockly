58d3a282371d510bb5be9e3dc25f64c6c5a21e59
Fedor Baart <fedor.baart@deltares.nl>
2019-08-09 16:45:01 +0200

    Cleanup

--- src/blockly.js
+++ src/blockly.js
@@ -3,14 +3,15 @@
 // Import and use export module to extract Blockly variable
 // eslint-disable-next-line import/no-webpack-loader-syntax
 var { Blockly, goog } = require('blockly/blockly_compressed.js')
-// Expose it globaly so it can be changed by  other Blocks  import
-console.log('Blockly, goog', Blockly, goog)
 // set to the windows object, so they are loaded in the next imports
+
 window.Blockly = Blockly
 window.goog = goog
 
+
 // Import with side effects
 // eslint-disable-next-line import/no-webpack-loader-syntax
+// this does not work in node testing (global goog is not available)
 require('blockly/msg/js/en.js')
 // eslint-disable-next-line import/no-webpack-loader-syntax
 require('blockly/blocks_compressed.js')
