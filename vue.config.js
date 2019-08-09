c7f612abd590c406b544b50f725c869e7e40db9f
Fedor Baart <fedor.baart@deltares.nl>
2019-08-09 14:30:15 +0200

    don't use method chaining, generates wrong config

--- vue.config.js
+++ vue.config.js
@@ -2,27 +2,22 @@ module.exports = {
   configureWebpack: {
     output: {
       libraryExport: 'default'
+    },
+    module: {
+      rules: [
+        {
+          test: /blockly\/blocks_compressed\.js/,
+          use: 'exports-loader?Blockly,goog'
+        },
+        {
+          test: /blockly\/blockly_compressed\.js/,
+          use: 'imports-loader?Blockly=>window.Blockly,goog=>window.goog'
+        },
+        {
+          test: /blockly\/msg\/js\/en.js/,
+          use: 'imports-loader?Blockly=>window.Blockly,goog=>window.goog'
+        }
+      ]
     }
-  },
-  chainWebpack: config => {
-    // load exports
-    config.module
-      .rule('export')
-      .test(/blockly\/blockly_compressed\.js/)
-      .use('exports-loader?Blockly,goog')
-      .loader('exports-loader')
-      .end()
-    config.module
-      .rule('import')
-      .test(/blockly\/blocks_compressed\.js/)
-      .use('imports-loader?Blockly=>window.Blockly,goog=>window.goog')
-      .loader('imports-loader')
-      .end()
-    config.module
-      .rule('import')
-      .test(/blockly\/msg\/js\/en.js/)
-      .use('imports-loader?Blockly=>window.Blockly,goog=>window.goog')
-      .loader('imports-loader')
-      .end()
   }
 }
